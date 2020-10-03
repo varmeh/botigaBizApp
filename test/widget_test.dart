18c03e0717e6eb03c0068bd9157e8b42166fd4d1
Varun Mehta <varun@botiga.app>
2020-10-03 11:48:25 +0530

    removing unused folder lib/lib & lib/app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:botiga_biz/main.dart';
+import 'package:botiga_biz/lib/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
