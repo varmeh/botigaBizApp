f2dbf5f5de72d60d90bc08fac7a70dbdde7276ae
Varun Mehta <varun@botiga.app>
2020-10-11 16:32:31 +0530

    removed dead code

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -1,19 +1,4 @@
-import 'package:flutter/material.dart';
-
 class Constants {
-  // TextStyle Constants
-  static const kTextStyleFontWeight300 = TextStyle(fontWeight: FontWeight.w300);
-  static const kTextStyleFontWeight600 = TextStyle(fontWeight: FontWeight.w600);
-  static const kTextStyleFontWeight700 = TextStyle(fontWeight: FontWeight.w700);
-
-  // EdgeInsets
-  static const kEdgeInsetsCard = EdgeInsets.only(
-    top: 25.0,
-    left: 15.0,
-    right: 15.0,
-    bottom: 10.0,
-  );
-
   //Order String Constants
   static const rupeeSymbol = '\u20B9';
   static const revenue = 'REVENUE';
