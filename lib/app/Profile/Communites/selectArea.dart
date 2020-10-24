af5b25e62567d3d714a8838b78bdbd7617cd3819
Rahul <Rahul@botiga.app>
2020-10-24 14:52:09 -0700

    Profile chages

--- lib/app/Profile/Communites/selectArea.dart
+++ lib/app/Profile/Communites/selectArea.dart
@@ -1,7 +1,10 @@
-import 'package:botiga_biz/widget/SearchBar.dart';
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'selectCommunites.dart';
+import '../../../util/index.dart';
 import '../../../theme/index.dart';
+import '../../../widget/index.dart';
+import '../../../util/network/index.dart' show HttpService;
 
 class SelectArea extends StatefulWidget {
   static const routeName = '/select-area';
@@ -10,23 +13,81 @@ class SelectArea extends StatefulWidget {
 }
 
 class _SelectAreaState extends State<SelectArea> {
-  final areas = [
-    "Jayanagar",
-    "Basavanagudi",
-    "Jayanagar",
-    "Basavanagudi",
-    "Jayanagar",
-    "Basavanagudi",
-    "Jayanagar",
-    "Basavanagudi",
-    "Jayanagar",
-    "Basavanagudi",
-    "Jayanagar",
-    "Basavanagudi",
-  ];
+  List<String> _searchResult = [];
+  List<String> _areas = [];
+  TextEditingController controller = new TextEditingController();
+  bool _isLoading, _isError, _isInit;
+  var _error;
+  String _searchQuery;
+
+  @override
+  void initState() {
+    super.initState();
+    _isLoading = false;
+    _isError = false;
+    _error = null;
+    _isInit = false;
+    _searchQuery = '';
+  }
+
+  @override
+  void didChangeDependencies() {
+    if (!_isInit) {
+      getAreasForcity().then((response) {
+        setState(() {
+          _isLoading = false;
+          for (final area in response) {
+            _areas.add(area);
+          }
+        });
+      }).catchError((err) {
+        setState(() {
+          _isLoading = false;
+          _isError = true;
+          _error = err;
+        });
+      });
+      _isInit = true;
+    }
+    super.didChangeDependencies();
+  }
+
+  Future getAreasForcity() async {
+    setState(() {
+      _isLoading = true;
+      _isError = false;
+      _error = null;
+    });
+    return await HttpService().get('${Constants.CITY_AREAS}');
+  }
+
+  Widget getTile(String txt) {
+    return ListTile(
+      onTap: () {
+        Navigator.of(context).pushNamed(SelectCommunites.routeName);
+      },
+      contentPadding: EdgeInsets.only(left: 20, right: 20),
+      title: Text(txt, style: AppTheme.textStyle.color100.size(17).w500),
+    );
+  }
+
+  void onSearchTextChanged(String text) async {
+    setState(() {
+      _searchQuery = text;
+    });
+    _searchResult.clear();
+    if (text.isEmpty) {
+      return null;
+    }
+    _areas.forEach((area) {
+      if (area.contains(text) || area.contains(text)) _searchResult.add(area);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppTheme.backgroundColor,
       appBar: AppBar(
         backgroundColor: AppTheme.backgroundColor,
         elevation: 0,
@@ -43,47 +104,74 @@ class _SelectAreaState extends State<SelectArea> {
         title: Align(
           child: Text(
             'Select Area',
-            style: AppTheme.textStyle.w700.color100.size(22).lineHeight(1.0),
+            style: AppTheme.textStyle.w700.color100.size(22).lineHeight(1.2),
           ),
           alignment: Alignment.centerLeft,
         ),
       ),
       body: SafeArea(
-        child: Container(
-          color: AppTheme.backgroundColor,
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(top: 20, bottom: 20),
-                child: SearchBar(
-                  placeholder: "Search...",
-                  onSubmit: null,
-                  onChange: null,
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: AppTheme.color05,
+                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                ),
+                child: ListTile(
+                  title: TextField(
+                    cursorHeight: 16.0,
+                    cursorColor: AppTheme.color05,
+                    textInputAction: TextInputAction.search,
+                    controller: controller,
+                    decoration: InputDecoration(
+                        hintText: 'Search...',
+                        hintStyle: AppTheme.textStyle.w500.color50
+                            .size(15.0)
+                            .lineHeight(1.3),
+                        border: InputBorder.none),
+                    onChanged: onSearchTextChanged,
+                  ),
+                  trailing: _searchQuery == ''
+                      ? Icon(BotigaIcons.search)
+                      : GestureDetector(
+                          child: Icon(Icons.clear),
+                          onTap: () {
+                            controller.clear();
+                            onSearchTextChanged('');
+                          },
+                        ),
                 ),
               ),
-              Expanded(
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: areas.length,
-                  itemBuilder: (context, index) {
-                    return getTile(areas[index], context);
-                  },
-                ),
-              ),
-            ],
-          ),
+            ),
+            Expanded(
+              child: _isLoading
+                  ? Loader()
+                  : _isError
+                      ? HttpServiceExceptionWidget(
+                          exception: _error,
+                          onTap: () {
+                            getAreasForcity();
+                          },
+                        )
+                      : _searchResult.length != 0 || controller.text.isNotEmpty
+                          ? ListView.builder(
+                              itemCount: _searchResult.length,
+                              itemBuilder: (context, i) {
+                                return getTile(_searchResult[i]);
+                              },
+                            )
+                          : ListView.builder(
+                              itemCount: _areas.length,
+                              itemBuilder: (context, index) {
+                                return getTile(_areas[index]);
+                              },
+                            ),
+            ),
+          ],
         ),
       ),
     );
   }
-}
-
-Widget getTile(String txt, BuildContext context) {
-  return ListTile(
-    onTap: () {
-      Navigator.of(context).pushNamed(SelectCommunites.routeName);
-    },
-    contentPadding: EdgeInsets.only(left: 20, right: 20),
-    title: Text(txt, style: AppTheme.textStyle.color100.size(17).w500),
-  );
 }
