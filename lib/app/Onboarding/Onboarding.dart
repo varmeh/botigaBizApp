0380ebc912c56774996b6b3680df11a5f61d8cd0
Varun Mehta <varun@botiga.app>
2020-10-30 15:40:32 +0530

    fixed onboarding screen

--- lib/app/Onboarding/Onboarding.dart
+++ lib/app/Onboarding/Onboarding.dart
@@ -4,102 +4,77 @@ import 'package:intro_slider/intro_slider.dart';
 import 'package:intro_slider/slide_object.dart';
 import '../Auth/Signup/SignUpWelcome.dart';
 
-class IntroScreen extends StatefulWidget {
+class IntroScreen extends StatelessWidget {
   static const routeName = '/intro';
-  IntroScreen({Key key}) : super(key: key);
-  @override
-  IntroScreenState createState() => new IntroScreenState();
-}
 
-class IntroScreenState extends State<IntroScreen> {
-  List<Slide> slides = new List();
+  final List<Slide> _slides = [
+    Slide(
+      title: "Reach more customers",
+      description:
+          "Vivamus amet, quam egestas elementum purus, viverra sed volutpat placerat",
+      pathImage: "assets/images/intro_one.png",
+    ),
+    Slide(
+      title: "Reach more customers",
+      description:
+          "Vivamus amet, quam egestas elementum purus, viverra sed volutpat placerat",
+      pathImage: "assets/images/intro_one.png",
+    ),
+    Slide(
+      title: "Reach more customers",
+      description:
+          "Vivamus amet, quam egestas elementum purus, viverra sed volutpat placerat",
+      pathImage: "assets/images/intro_one.png",
+    ),
+  ];
 
-  @override
-  void initState() {
-    super.initState();
-
-    slides.add(
-      new Slide(
-        title: "Reach more customers",
-        description:
-            "Vivamus amet, quam egestas elementum purus, viverra sed volutpat placerat",
-        pathImage: "assets/images/intro_one.png",
-      ),
-    );
-    slides.add(
-      new Slide(
-        title: "Reach more customers",
-        description:
-            "Vivamus amet, quam egestas elementum purus, viverra sed volutpat placerat",
-        pathImage: "assets/images/intro_one.png",
-      ),
-    );
-    slides.add(
-      new Slide(
-        title: "Reach more customers",
-        description:
-            "Vivamus amet, quam egestas elementum purus, viverra sed volutpat placerat",
-        pathImage: "assets/images/intro_one.png",
-      ),
-    );
-  }
-
-  void onDonePress() {
-    Navigator.pushReplacement(context,
-        MaterialPageRoute(builder: (BuildContext context) => SignupWelcome()));
-  }
-
-  List<Widget> renderListCustomTabs() {
-    List<Widget> tabs = new List();
-    for (int i = 0; i < slides.length; i++) {
-      Slide currentSlide = slides[i];
-      tabs.add(Container(
-          width: double.infinity,
-          height: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              GestureDetector(
-                  child: Image.asset(
-                currentSlide.pathImage,
-                width: 331.0,
-                height: 246.0,
-                fit: BoxFit.contain,
-              )),
-              SizedBox(
-                height: 100,
+  Widget _customTab(Slide slide) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          GestureDetector(
+              child: Image.asset(
+            slide.pathImage,
+            width: 331.0,
+            height: 246.0,
+            fit: BoxFit.contain,
+          )),
+          SizedBox(
+            height: 100,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: Container(
+              width: double.infinity,
+              child: Text(
+                slide.title,
+                style: AppTheme.textStyle.w700.size(22).color100,
+                textAlign: TextAlign.left,
               ),
-              Padding(
-                padding: const EdgeInsets.only(left: 20, right: 20),
-                child: Container(
-                  width: double.infinity,
-                  child: Text(
-                    currentSlide.title,
-                    style: AppTheme.textStyle.w700.size(22).color100,
-                    textAlign: TextAlign.left,
-                  ),
-                  margin: EdgeInsets.only(top: 20.0),
+              margin: EdgeInsets.only(top: 20.0),
+            ),
+          ),
+          Flexible(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: Container(
+                width: double.infinity,
+                child: Text(
+                  slide.description,
+                  style: AppTheme.textStyle.w500.size(15).color50,
+                  textAlign: TextAlign.left,
+                  maxLines: 3,
                 ),
+                margin: EdgeInsets.only(top: 20.0),
               ),
-              Padding(
-                padding: const EdgeInsets.only(left: 20, right: 20),
-                child: Container(
-                  width: double.infinity,
-                  child: Flexible(
-                    child: Text(
-                      currentSlide.description,
-                      style: AppTheme.textStyle.w500.size(15).color50,
-                      textAlign: TextAlign.left,
-                      maxLines: 3,
-                    ),
-                  ),
-                  margin: EdgeInsets.only(top: 20.0),
-                ),
-              ),
-            ],
-          )));
-    }
-    return tabs;
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
@@ -108,14 +83,21 @@ class IntroScreenState extends State<IntroScreen> {
       backgroundColor: AppTheme.backgroundColor,
       body: SafeArea(
         child: IntroSlider(
-            //backgroundColorAllSlides: AppTheme.primaryColor,
-            slides: this.slides,
-            colorDot: AppTheme.primaryColor,
-            onDonePress: this.onDonePress,
-            listCustomTabs: this.renderListCustomTabs(),
-            colorSkipBtn: AppTheme.primaryColor,
-            colorDoneBtn: AppTheme.primaryColor,
-            highlightColorDoneBtn: AppTheme.primaryColor),
+          slides: this._slides,
+          colorDot: AppTheme.color25,
+          colorActiveDot: AppTheme.primaryColor,
+          onDonePress: () {
+            Navigator.of(context).pushNamed(SignupWelcome.routeName);
+          },
+          listCustomTabs: _slides.map((slide) => _customTab(slide)).toList(),
+          colorSkipBtn: AppTheme.color05,
+          styleNameSkipBtn: AppTheme.textStyle.w500.size(15).color100,
+          colorDoneBtn: AppTheme.primaryColor,
+          styleNameDoneBtn: AppTheme.textStyle.w500
+              .size(15)
+              .colored(AppTheme.backgroundColor),
+          highlightColorDoneBtn: AppTheme.primaryColor,
+        ),
       ),
     );
   }
