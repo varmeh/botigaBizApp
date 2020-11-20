fe944bbeb688736d6c9fb7efc33c019701c613f1
Varun Mehta <varun@botiga.app>
2020-11-20 19:14:36 +0530

    add product animation changed

--- lib/app/store/storeScreen.dart
+++ lib/app/store/storeScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:animations/animations.dart';
+
 import '../../theme/index.dart';
 import '../../widget/index.dart';
 import '../../util/index.dart';
@@ -14,7 +16,7 @@ class StoreScreen extends StatefulWidget {
 
 class _StoreScreenState extends State<StoreScreen>
     with SingleTickerProviderStateMixin {
-  int slelectedTab = 0;
+  int selectedTab = 0;
   bool _isLoading = false;
   bool _isError = false;
   bool _isInit = false;
@@ -31,9 +33,7 @@ class _StoreScreenState extends State<StoreScreen>
     _categoryNameFocusNode = FocusNode();
     _controller = TabController(length: tabList.length, vsync: this);
     _controller.addListener(() {
-      setState(() {
-        slelectedTab = _controller.index;
-      });
+      setState(() => selectedTab = _controller.index);
     });
   }
 
@@ -169,134 +169,154 @@ class _StoreScreenState extends State<StoreScreen>
   }
 
   Widget _floatingButton(BuildContext context) {
-    final bool hasCategories =
-        Provider.of<CategoryProvider>(context, listen: true)
-                .allCategories
-                .length >
-            0;
-    return (slelectedTab == 0 && !hasCategories)
-        ? SizedBox.shrink()
-        : Container(
-            padding: EdgeInsets.only(bottom: 32.0),
+    if (selectedTab == 1) {
+      return _addCategory();
+    } else {
+      final hasCategories = Provider.of<CategoryProvider>(context, listen: true)
+              .allCategories
+              .length >
+          0;
+
+      return hasCategories ? _addProduct() : SizedBox.shrink();
+    }
+  }
+
+  Widget _addProduct() {
+    return OpenContainer(
+        closedElevation: 0.0,
+        transitionDuration: Duration(milliseconds: 500),
+        closedBuilder: (context, openContainer) {
+          return Container(
+            color: Colors.transparent,
+            margin: const EdgeInsets.only(bottom: 32.0),
+            padding: const EdgeInsets.symmetric(horizontal: 2.0),
             child: FloatingActionButton.extended(
-              backgroundColor: AppTheme.backgroundColor,
               elevation: 4.0,
+              backgroundColor: AppTheme.backgroundColor,
               icon: const Icon(Icons.add, color: Color(0xff179F57)),
-              label: slelectedTab == 0
-                  ? Text('ADD PRODUCT',
-                      style: AppTheme.textStyle
-                          .colored(AppTheme.primaryColor)
-                          .w700
-                          .size(12)
-                          .letterSpace(1))
-                  : Text(
-                      'ADD CATEGORY',
-                      style: AppTheme.textStyle
-                          .colored(AppTheme.primaryColor)
-                          .w700
-                          .size(12)
-                          .letterSpace(1),
+              label: Text(
+                'ADD PRODUCT',
+                style: AppTheme.textStyle
+                    .colored(AppTheme.primaryColor)
+                    .w700
+                    .size(12)
+                    .letterSpace(1),
+              ),
+              onPressed: () => openContainer(),
+            ),
+          );
+        },
+        openBuilder: (_, __) => AddProduct());
+  }
+
+  Widget _addCategory() {
+    return Container(
+      padding: EdgeInsets.only(bottom: 32.0),
+      child: FloatingActionButton.extended(
+        backgroundColor: AppTheme.backgroundColor,
+        elevation: 4.0,
+        icon: const Icon(Icons.add, color: Color(0xff179F57)),
+        label: Text(
+          'ADD CATEGORY',
+          style: AppTheme.textStyle
+              .colored(AppTheme.primaryColor)
+              .w700
+              .size(12)
+              .letterSpace(1),
+        ),
+        onPressed: () {
+          final _formKey = GlobalKey<FormState>();
+          String _categoryName = '';
+          BotigaBottomModal(
+            isDismissible: false,
+            child: Column(
+              children: [
+                Form(
+                  key: _formKey,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: AppTheme.backgroundColor,
+                      borderRadius: BorderRadius.only(
+                        topLeft: const Radius.circular(16.0),
+                        topRight: const Radius.circular(16.0),
+                      ),
                     ),
-              onPressed: () {
-                if (slelectedTab == 1) {
-                  final _formKey = GlobalKey<FormState>();
-                  String _categoryName = '';
-                  BotigaBottomModal(
-                    isDismissible: false,
                     child: Column(
-                      children: [
-                        Form(
-                          key: _formKey,
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: AppTheme.backgroundColor,
-                              borderRadius: BorderRadius.only(
-                                topLeft: const Radius.circular(16.0),
-                                topRight: const Radius.circular(16.0),
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Text(
+                          'Add category',
+                          style: AppTheme.textStyle.color100.w700.size(22),
+                        ),
+                        SizedBox(
+                          height: 24,
+                        ),
+                        BotigaTextFieldForm(
+                          focusNode: _categoryNameFocusNode,
+                          labelText: 'Category name',
+                          onSave: (value) => _categoryName = value,
+                          validator: nameValidator,
+                        ),
+                        SizedBox(
+                          height: 40,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            FlatButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              highlightColor: Colors.transparent,
+                              splashColor: Colors.transparent,
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.only(top: 14, bottom: 14),
+                                child: Text(
+                                  'Cancel',
+                                  style:
+                                      AppTheme.textStyle.color50.w600.size(15),
+                                ),
                               ),
                             ),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisSize: MainAxisSize.min,
-                              children: <Widget>[
-                                Text(
-                                  'Add category',
-                                  style:
-                                      AppTheme.textStyle.color100.w700.size(22),
-                                ),
-                                SizedBox(
-                                  height: 24,
-                                ),
-                                BotigaTextFieldForm(
-                                  focusNode: _categoryNameFocusNode,
-                                  labelText: 'Category name',
-                                  onSave: (value) => _categoryName = value,
-                                  validator: nameValidator,
-                                ),
-                                SizedBox(
-                                  height: 40,
-                                ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  children: <Widget>[
-                                    FlatButton(
-                                      onPressed: () {
-                                        Navigator.of(context).pop();
-                                      },
-                                      highlightColor: Colors.transparent,
-                                      splashColor: Colors.transparent,
-                                      child: Padding(
-                                        padding: const EdgeInsets.only(
-                                            top: 14, bottom: 14),
-                                        child: Text('Cancel',
-                                            style: AppTheme
-                                                .textStyle.color50.w600
-                                                .size(15)),
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      width: 15,
-                                    ),
-                                    FlatButton(
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(8.0),
-                                      ),
-                                      onPressed: () {
-                                        if (_formKey.currentState.validate()) {
-                                          _formKey.currentState.save();
-                                          _handleCategorySave(_categoryName);
-                                        }
-                                      },
-                                      color: AppTheme.primaryColor,
-                                      highlightColor:
-                                          AppTheme.primaryColorVariant,
-                                      child: Padding(
-                                        padding: const EdgeInsets.only(
-                                            top: 14, bottom: 14),
-                                        child: Text('Save category',
-                                            style: AppTheme.textStyle
-                                                .colored(
-                                                    AppTheme.backgroundColor)
-                                                .w600
-                                                .size(15)),
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              ],
+                            SizedBox(
+                              width: 15,
                             ),
-                          ),
+                            FlatButton(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                              onPressed: () {
+                                if (_formKey.currentState.validate()) {
+                                  _formKey.currentState.save();
+                                  _handleCategorySave(_categoryName);
+                                }
+                              },
+                              color: AppTheme.primaryColor,
+                              highlightColor: AppTheme.primaryColorVariant,
+                              child: Padding(
+                                padding:
+                                    const EdgeInsets.only(top: 14, bottom: 14),
+                                child: Text('Save category',
+                                    style: AppTheme.textStyle
+                                        .colored(AppTheme.backgroundColor)
+                                        .w600
+                                        .size(15)),
+                              ),
+                            )
+                          ],
                         ),
                       ],
                     ),
-                  ).show(context);
-                } else {
-                  Navigator.of(context).pushNamed(AddProduct.routeName);
-                }
-              },
+                  ),
+                ),
+              ],
             ),
-          );
+          ).show(context);
+        },
+      ),
+    );
   }
 }
